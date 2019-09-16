FFmpeg/FFV1

commit 446c4e41a1311ae7931db9a18b8ae3387af1d382

refactor the Makefile target handling for the RFC XML dependency

Makefile
@@ -8,29 +8,29 @@ $(info RFC rendering has been tested with mmark version 2.0.48, xml2rfc 2.23.1, 
 
 all: draft-ietf-cellar-ffv1-$(VERSION).html draft-ietf-cellar-ffv1-v4-$(VERSION-v4).html draft-ietf-cellar-ffv1-$(VERSION).txt draft-ietf-cellar-ffv1-v4-$(VERSION-v4).txt
 
-draft-ietf-cellar-ffv1-$(VERSION).html: ffv1.md
+draft-ietf-cellar-ffv1-$(VERSION).xml: ffv1.md
 	bash makesvg
 	cat rfc_frontmatter.md "$<" rfc_backmatter.md | grep -v "^AART:" | grep -v "^SVGC" | grep -v "{V4}" |  sed "s|^AART:||g;s|{V3}||g;s|SVGI:||g" > merged_rfchtml.md
 	mmark merged_rfchtml.md > draft-ietf-cellar-ffv1-$(VERSION).xml
 	bash svg2src draft-ietf-cellar-ffv1-$(VERSION).xml
-	xml2rfc --html --v3 draft-ietf-cellar-ffv1-$(VERSION).xml -o "$@"
 
-draft-ietf-cellar-ffv1-v4-$(VERSION-v4).html: ffv1.md
+draft-ietf-cellar-ffv1-v4-$(VERSION-v4).xml: ffv1.md
 	bash makesvg
-	cat rfc_frontmatter.md "$<" rfc_backmatter.md | grep -v "^AART:" | grep -v "^SVGC" | grep -v "{V3}" | sed "s|^AART:||g;s|{V4}||g;s|SVGI:||g" > merged_rfchtml-v4.md
-	mmark merged_rfchtml-v4.md > draft-ietf-cellar-ffv1-v4-$(VERSION-v4).xml
+	cat rfc_frontmatter.md "$<" rfc_backmatter.md | grep -v "^AART:" | grep -v "^SVGC" | grep -v "{V3}" |  sed "s|^AART:||g;s|{V4}||g;s|SVGI:||g" > merged_rfchtml-v4.md
+	mmark merged_rfchtml.md > draft-ietf-cellar-ffv1-v4-$(VERSION-v4).xml
 	bash svg2src draft-ietf-cellar-ffv1-v4-$(VERSION-v4).xml
-	xml2rfc --html --v3 draft-ietf-cellar-ffv1-v4-$(VERSION-v4).xml -o "$@"
 
-draft-ietf-cellar-ffv1-$(VERSION).txt: ffv1.md
-	cat rfc_frontmatter.md "$<" rfc_backmatter.md | grep -v "^SVG" | grep -v "{V4}" | sed "s|^AART:||g;s|{V3}||g" > merged_rfctxt.md
-	mmark merged_rfctxt.md > draft-ietf-cellar-ffv1-$(VERSION).xml
-	xml2rfc --v3 draft-ietf-cellar-ffv1-$(VERSION).xml -o "$@"
+draft-ietf-cellar-ffv1-$(VERSION).html: draft-ietf-cellar-ffv1-$(VERSION).xml
+	xml2rfc --html --v3 "$<" -o "$@"
 
-draft-ietf-cellar-ffv1-v4-$(VERSION-v4).txt: ffv1.md
-	cat rfc_frontmatter.md "$<" rfc_backmatter.md | grep -v "^SVG" | grep -v "{V3}" | sed "s|^AART:||g;s|{V4}||g" > merged_rfctxt-v4.md
-	mmark merged_rfctxt-v4.md > draft-ietf-cellar-ffv1-v4-$(VERSION-v4).xml
-	xml2rfc --v3 draft-ietf-cellar-ffv1-v4-$(VERSION-v4).xml -o "$@"
+draft-ietf-cellar-ffv1-v4-$(VERSION-v4).html: draft-ietf-cellar-ffv1-v4-$(VERSION-v4).xml
+	xml2rfc --html --v3 "$<" -o "$@"
+
+draft-ietf-cellar-ffv1-$(VERSION).txt: draft-ietf-cellar-ffv1-$(VERSION).xml
+	xml2rfc --v3 "$<" -o "$@"
+
+draft-ietf-cellar-ffv1-v4-$(VERSION-v4).txt: draft-ietf-cellar-ffv1-v4-$(VERSION-v4).xml
+	xml2rfc --v3 "$<" -o "$@"
 
 clean:
 	rm -f ffv1.pdf ffv1-v4.pdf ffv1.html ffv1-v4.html draft-ietf-cellar-ffv1-* merged_*

commit f13bab35ce3b2ad2483c14c60b86843e730ee856

fix typo in Makefile for ffv1 version 4 doc
regression since https://github.com/FFmpeg/FFV1/commit/446c4e41a1311ae7931db9a18b8ae3387af1d382

Makefile
@@ -17,7 +17,7 @@ draft-ietf-cellar-ffv1-$(VERSION).xml: ffv1.md
 draft-ietf-cellar-ffv1-v4-$(VERSION-v4).xml: ffv1.md
 	bash makesvg
 	cat rfc_frontmatter.md "$<" rfc_backmatter.md | grep -v "^AART:" | grep -v "^SVGC" | grep -v "{V3}" |  sed "s|^AART:||g;s|{V4}||g;s|SVGI:||g" > merged_rfchtml-v4.md
-	mmark merged_rfchtml.md > draft-ietf-cellar-ffv1-v4-$(VERSION-v4).xml
+	mmark merged_rfchtml-v4.md > draft-ietf-cellar-ffv1-v4-$(VERSION-v4).xml
 	bash svg2src draft-ietf-cellar-ffv1-v4-$(VERSION-v4).xml
 
 draft-ietf-cellar-ffv1-$(VERSION).html: draft-ietf-cellar-ffv1-$(VERSION).xml